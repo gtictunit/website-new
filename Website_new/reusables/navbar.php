93cf73500feb05c8ec8208978097338192e08664
Kwesi Hervie <herviek2001@gmail.com>
2023-03-09 17:30:35 +0100

    new changes

---
+++ Website_new/reusables/navbar.php
@@ -0,0 +1,34 @@
+<nav class="nav navbar navbar-expand-lg">
+    <div>
+        <a href="/" class="navbar-brand">
+            <img src="/assets/images/logo.png" alt="Logo" width="75" height="auto" class="d-inline-block align-text-top"
+                alt="" />
+            <div>Glory Tabernacle Ministry</div>
+        </a>
+    </div>
+    <div class="">
+        <ul class="nav-list">
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Home </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Home </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Home </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Home </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Home </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Home </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Home </a>
+            </li>
+        </ul>
+    </div>
+</nav>
