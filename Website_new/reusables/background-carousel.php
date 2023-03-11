86a0a0f3c2181243ec8f5d3cd65de4315ee01ea1
gtict <gtict2014@gmai.com>
2023-03-11 06:11:01 +0100

    refactors

---
+++ Website_new/reusables/background-carousel.php
@@ -0,0 +1,11 @@
+<?php echo'
+        <ul class="cb-slideshow header">
+        <div class="overlay"></div>
+            <li><span>Image 01</span><div><h3></h3></div></li>
+            <li><span>Image 02</span><div><h3></h3></div></li>
+            <li><span>Image 03</span><div><h3></h3></div></li>
+            <li><span>Image 04</span><div><h3></h3></div></li>
+            <li><span>Image 05</span><div><h3></h3></div></li>
+            <li><span>Image 06</span><div><h3></h3></div></li>
+        </ul>'
+?>
