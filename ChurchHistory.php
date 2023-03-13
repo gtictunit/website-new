4284a398c4dd684090b4cb03967f9d38c958af78
f3ranmi <ibitoyeferanmi70@gmail.com>
2023-03-13 11:48:03 +0100

    new

--- ChurchHistory.php
+++ ChurchHistory.php
@@ -149,39 +149,8 @@
       </div>
     </div>
     <center>
-      <div class="images-coro container">
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-              aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-              aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-              aria-label="Slide 3"></button>
-          </div>
-          <div class="carousel-inner img-coro">
-            <div class="carousel-item active">
-              <img src="./assets/images/bgcolor.jpg" class="d-block w-75" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="./assets/images/crd-bg-video.jpg" class="d-block w-75" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="./assets/images/crd-welcome.jpg" class="d-block w-75" alt="...">
-            </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div>
+      <h2>This is for the video of the biography</h2>
+     <video src="./assets/images/AnimePahe_Shingeki_no_Kyojin_-_74_BD_720p_SCY.mp4" controls width="700"></video>
     </center>
 
 
