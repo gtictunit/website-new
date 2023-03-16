225d9de61f71a12eb46a91b511a24644b3154e6b
Noah <noahtochukwu10@gmail.com>
2023-03-16 11:52:48 +0100

    Weekly programs issue

--- weekly-programmes.php
+++ weekly-programmes.php
@@ -225,113 +225,302 @@
         </button>
       </div>
       <br>
+    
+
       <style>
-        
-     body{
-        font-family: "Josefin Sans", sans-serif ;}
-        
-        .head{
+        .nav {
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  /* box-shadow: none; */
+  padding: 10px 30px;
+  background-color: #3d0052;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  color: white;
+}
+
+/* .navbar {
+  background-color: rgba(0, 0, 0, 0) !important;
+  transition-property: background-color, color;
+  transition-duration: 0.5s;
+  transition-function: ease-in-out;
+} */
+
+/* .navbar-scrolled {
+  background-color: #3d0052 !important;
+  color: #ffffff !important;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+} */
+
+.nav-tgl {
+  display: none;
+}
+.navbar-brand {
+  font-weight: 700;
+  font-size: 20px;
+  font-family: "Ubuntu", sans-serif;
+  display: flex;
+  align-items: center;
+  color: white;
+  gap: 10px;
+}
+
+.navbar-brand:hover {
+  color: white;
+  opacity: 0.9;
+}
+
+.nav-list {
+  display: flex;
+  gap: 4px;
+  align-items: center;
+  list-style: none;
+  margin: 0;
+}
+
+.nav-link {
+  color: white;
+  font-weight: 600;
+  font-family: "Montserrat", sans-serif;
+  text-decoration: none;
+  display: inline-block;
+  padding: 15px 0;
+  position: relative;
+  margin: 0 20px;
+  font-size: 15px;
+}
+
+.nav-link:hover {
+  color: yellow;
+}
+
+/* .active {
+  color: white;
+  border-bottom: 2px solid white;
+} */
+
+/* .menu-icon {
+  display: none;
+} */
+
+.nav-link:after {
+  background: none repeat scroll 0 0 transparent;
+  bottom: 0;
+  content: "";
+  display: block;
+  height: 2px;
+  left: 50%;
+  position: absolute;
+  background: yellow;
+  transition: width 0.3s ease 0s, left 0.3s ease 0s;
+  width: 0;
+}
+.nav-link:hover:after {
+  width: 100%;
+  left: 0;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+.dropdown-btn {
+  display: flex;
+  gap: 6px;
+  align-items: center;
+  cursor: pointer;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #fff;
+  min-width: 300px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  /* margin-top: -10px; */
+  border-radius: 8px;
+  max-height: 0;
+  -webkit-transition: all 0.5s ease-in-out 0s;
+  transition: all 0.5s ease-in-out 0s;
+  padding: 20px;
+  
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  font-family: "Ubuntu";
+  font-size: 16px;
+  font-weight: 500;
+  right: 0px;
+}
+.missions-drop{
+  right: 0px;
+}
+.dropdown-content a:hover {
+  background-color: #3d0052;
+  color: #fff;
+  border-radius: 8px;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+  max-height: 500px;
+  opacity: 1;
+  z-index: 9999;
+}
+
+.dropdown:hover .dropbtn {
+  /* background-color: #3e8e41; */
+}
+.overlay {
+  background: rgba(0, 0, 0, 0.6);
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  /* z-index: 200; */
+}
+
+.main-header {
+  height: 100vh !important;
+  background-image: url("../assets/images/heroo.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.hero {
+  position: relative;
+  z-index: 1000;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.header-description {
+  font-size: 22px;
+  margin-top: 48px;
+  display: flex;
+  gap: 22px;
+}
+
+.header-text {
+  font-size: 100px;
+  font-family: "Josefin Sans", sans-serif;
+  text-align: center;
+}
+        body{
+           font-family: "Josefin Sans", sans-serif ;}
+           
+           .head{
+               max-width: 100%;
+               color: #101318;
+                           font-size: 4vw;
+               font-weight: 700;
+               line-height: 1.2em
+           }
+           
+           .carousel-item > div {
+             display: flex;
+             justify-content: space-between;
+           }
+           
+           @media (max-width: 767px) {
+             .carousel-item > div > .col-md-4:nth-child(n+4) {
+               display: none;
+             }
+           }
+           
+           @media (min-width: 768px) and (max-width: 991px) {
+             .carousel-item > div > .col-md-4:nth-child(n+3) {
+               display: none;
+             }
+           }
+           .carousel-control-prev, .carousel-control-next {
+            opacity: 0;
+            transition: opacity .9s ease-in-out;
+            
+            
+           }   
+   
+           #carouselExample:hover .carousel-control-prev,
+           #carouselExample:hover .carousel-control-next {
+            opacity: 1;
+            
+           }
+           .hover-effect {
+            transition: all 2s ease-in-out;
+            height: auto;
             max-width: 100%;
-            color: #101318;
-                        font-size: 4vw;
-            font-weight: 700;
-            line-height: 1.2em
-        }
-        
-        .carousel-item > div {
-          display: flex;
-          justify-content: space-between;
-        }
-        
-        @media (max-width: 767px) {
-          .carousel-item > div > .col-md-4:nth-child(n+4) {
-            display: none;
-          }
-        }
-        
-        @media (min-width: 768px) and (max-width: 991px) {
-          .carousel-item > div > .col-md-4:nth-child(n+3) {
-            display: none;
-          }
-        }
-        .carousel-control-prev, .carousel-control-next {
-         opacity: 0;
-         transition: opacity .9s ease-in-out;
-         
-         
-        }   
-
-        #carouselExample:hover .carousel-control-prev,
-        #carouselExample:hover .carousel-control-next {
-         opacity: 1;
-         
-        }
-        .hover-effect {
-         transition: all 2s ease-in-out;
-         height: auto;
-         max-width: 100%;
-         
-        }
-        .hover-effect:hover {
-         transform: scale(1.1);
-        
-        }
-        .card-title{
-            font-weight: bold;
-            margin-bottom: 13px;
-            margin-left: 11px;
-        }
-        .card{
-            border: 0;
-            background-color: #ffffff;
-        }
-        .card, .img-container{
-            overflow: hidden;
-        
-        }
-        .desc{
-            margin-left: 11px;
-            font-weight: bold;
-            margin-bottom: 22px;
-        }
             
-        .si{
-            width: 100%;
-            height: 16.54em;
-            padding: 0%;
-            
-        }
-        .col-5{
-            padding-bottom: -100px;
-        }
-        .col-7{
-           margin-top: -7px;
-        }
-        .pill{
-            background-color: #8a5bf8;
-            font-size: 13px;
-            font-weight: bold;
-            width: 56.7px;
-            padding-bottom: 3px;
-            padding-top: 1px;
-        }
-        .pill2{
-            width: 56.7px;
-            padding-bottom: 5px;
-            font-weight: bold;
-        }
-        .ttt1, .ttt2{
-            margin-left: -115px;
-        }
-        .ttt1{
-            margin-top: 5px;
-        }
-        .ttt2{
-            font-size: 14px;
-            font-weight: bold;
-        }
-
-      </style>
-      
+           }
+           .hover-effect:hover {
+            transform: scale(1.1);
+           
+           }
+           .card-title{
+               font-weight: bold;
+               margin-bottom: 13px;
+               margin-left: 11px;
+           }
+           .card{
+               border: 0;
+               background-color: #ffffff;
+           }
+           .card, .img-container{
+               overflow: hidden;
+           
+           }
+           .desc{
+               margin-left: 11px;
+               font-weight: bold;
+               margin-bottom: 22px;
+           }
+               
+           .si{
+               width: 100%;
+               height: 16.54em;
+               padding: 0%;
+               
+           }
+           .col-5{
+               padding-bottom: -100px;
+           }
+           .col-7{
+              margin-top: -7px;
+           }
+           .pill{
+               background-color: #8a5bf8;
+               font-size: 13px;
+               font-weight: bold;
+               width: 56.7px;
+               padding-bottom: 3px;
+               padding-top: 1px;
+           }
+           .pill2{
+               width: 56.7px;
+               padding-bottom: 5px;
+               font-weight: bold;
+           }
+           .ttt1, .ttt2{
+               margin-left: -115px;
+           }
+           .ttt1{
+               margin-top: 5px;
+           }
+           .ttt2{
+               font-size: 14px;
+               font-weight: bold;
+           }
+   
+         </style>
 </body>
 </html>
